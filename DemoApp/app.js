66bdb44263088144836ca5110c81d67ea1e6515d
cuauhtlahuac <cuitlagua@gmail.com>
2018-02-19 05:41:39 +0000

    adding a new file

---
+++ DemoApp/app.js
@@ -0,0 +1,7 @@
+var cat = require('cat-me');
+var joke = require('knock-knock-jokes');
+var nan0 = require("number-is-nan");
+var camelCase0 = require("camelcase");
+
+console.log(nan0(0/0));
+console.log(camelCase0("estamos de fiesta"))
---
+++ EchoExercise/Echo.js
@@ -0,0 +1,8 @@
+function echo (str,num){
+    for(i=1;i <= num;i++){
+        console.log(str);
+    }
+}
+
+echo("Echo !!!",10);
+echo("TaterTots",3);
